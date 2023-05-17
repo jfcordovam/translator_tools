f18672252367ee88bc2869a17b7903c6190360d7
jfcordovam <jfcordovam@github.com>
2023-05-17 17:48:20 +0200

    Fix y mejoras de estilos

--- public/subtitleProcessor.js
+++ public/subtitleProcessor.js
@@ -1,4 +1,5 @@
 const REGEX1 = /\d*\n\d{2}:\d{2}:\d{2}\,\d{3} --> \d{2}:\d{2}:\d{2}\,\d{3}\n.*?(?=\n\n\d+.\d{2}:\d{2}:\d{2}\,\d{3} -->)/gsm;
+const LAST_DIALOGUE_REGEX = /.*(\d{4}\n\d{2}:\d{2}:\d{2}[.,]\d{3} --> \d{2}:\d{2}:\d{2}[.,]\d{3}\n.*?)$/gms;
 const REGEX2 = /\d*\n\d{2}:\d{2}:\d{2}[.,]\d{3} --> \d{2}:\d{2}:\d{2}[.,]\d{3}\n.*?(?=\n\n\d+.\d{2}:\d{2}:\d{2}[.,]\d{3} -->)/gsm;
 
 export default function processSubtitles(textContent, stepNr) {
@@ -6,7 +7,32 @@ export default function processSubtitles(textContent, stepNr) {
     let regex = (stepNr == 1) ? REGEX1 : REGEX2;
     let result = "";
 
-    textContent.match(regex).forEach(match => {
+    result += `${processMatches(regex, textContent)}`;
+
+    // fix: get last dialogue too
+    result += `${processMatches(LAST_DIALOGUE_REGEX, textContent)}`;
+
+    return result;
+}
+
+
+
+// Returns how many times a 'char' appears in 'text'
+function charCount(char, text) {
+
+    let arr = text.split(char);
+    return arr.length - 1;
+}
+
+// Returns whether this piece of text correspondes to more that one line of diialogue
+function isMultiLineDialogue(text) {
+    return charCount("-", text) >= 2
+}
+
+function processMatches(regex, text) {
+    let result = "";
+
+    text.match(regex).forEach(match => {
         let textsArray = match.split("\n");
 
         let dialogueNr = textsArray[0]; // "23"
@@ -28,18 +54,4 @@ export default function processSubtitles(textContent, stepNr) {
     });
 
     return result;
-}
-
-
-
-// Returns how many times a 'char' appears in 'text'
-function charCount(char, text) {
-
-    let arr = text.split(char);
-    return arr.length - 1;
-}
-
-// Returns whether this piece of text correspondes to more that one line of diialogue
-function isMultiLineDialogue(text) {
-    return charCount("-", text) >= 2
 }

cd60cca37769a2af612c23df213db775e6bc99f1
jfcordovam <jfcordovam@github.com>
2023-05-17 18:19:19 +0200

    Revert "Fix y mejoras de estilos"
    This reverts commit f18672252367ee88bc2869a17b7903c6190360d7.

--- script.js
+++ script.js
@@ -5,27 +5,6 @@ const form2 = document.querySelector('#form2');
 const fileInput = document.querySelector('#file-input-step1');
 const fileInput2 = document.querySelector('#file-input-step2');
 
-fileInput.addEventListener('change', (event) => checkFile(event, 1));
-fileInput2.addEventListener('change', (event) => checkFile(event, 2));
-
-function checkFile(event, buttonNumber) {
-  // Get the uploaded file
-  const file = event.target.files[0];
-
-  let button = document.querySelector('#upload-button-' + buttonNumber);
-
-  // Check if a file was uploaded successfully
-  if (file) {
-    // Change the button style to blue
-    button.classList.remove('grey');
-    button.classList.add('blue'); 
-    toggleOffErrorUpload(buttonNumber);
-  } else {
-    button.classList.add('grey');
-    button.classList.remove('blue'); 
-  }
-}
-
 form1.addEventListener('submit', (event) => {
     event.preventDefault();
     processFileEvent(fileInput, 1);
@@ -43,7 +22,6 @@ function processFileEvent(fileInput, stepNr) {
     const file = fileInput.files[0];
 
     if (file && file.type === 'text/plain') {
-        toggleOffErrorUpload(stepNr);
         const reader = new FileReader();
         reader.onload = (event) => {
 
@@ -54,35 +32,13 @@ function processFileEvent(fileInput, stepNr) {
 
             if (result) {
                 downloadFile(result, `result${stepNr}.txt`);
-                showSuccessStep(stepNr);
             }
 
         };
         reader.readAsText(file);
     } else {
-        showErrorUpload(stepNr);
         console.log('Please select a .txt file.');
-        showAlert('Por favor sube un archivo .txt')
     }
-}
-
-function showAlert(text){
-    window.alert(text);
-}
-
-function showErrorUpload(number){
-    let input = document.querySelector('#file-input-step' + number);
-    input.classList.add('error');
-}
-
-function toggleOffErrorUpload(number){
-    let input = document.querySelector('#file-input-step' + number);
-    input.classList.remove('error');
-}
-
-function showSuccessStep(stepNr) {
-    let step = document.querySelector('#step-' + stepNr);
-    step.classList.add('success');
 }
 
 function downloadFile(textContent, fileName) {
